8b6aad24927679cb68771248378ccbc1d19af5d4
SamMeg-stack <meg.samir1995@gmail.com>
2024-10-03 17:55:09 +0200

    fixed all errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,21 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-        config.module.rules.push({
-          test: /\.svg$/i,
-          issuer: /\.[jt]sx?$/,
-          use: ['@svgr/webpack'],
-        })
-    
-        return config
-      },
+  webpack(config) {
+    // Add the alias for '@' to point to 'src'
+    config.resolve.alias['@'] = path.resolve('src');
+
+    // Add the custom SVG handling rule
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
+
